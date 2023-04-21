d9d3489c49c39acda4d4ab1559f08c389474bc31
Yunji Ha <yunji118@ewhain.net>
2023-04-21 20:13:15 +0900

    Update printerror.c

--- printerror.c
+++ printerror.c
@@ -1,52 +1,61 @@
+/* printerror.c - Check identifier error
+* programmer - 김지윤, 김도연, 김원우, 하윤지
+* date - 2023/04/21
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "tn.h"
-#include "glob.h"
+
 
 #define isLetter(x) ((((x>='a'&&(x)<='z') || ((x)>='A'&&(x)<='Z')) || (x=='_')))
 #define isDigit(x) ((x) >= '0' && (x) <= '9')
 
-extern void PrintID();
+int nextid;
+int nextSTfree;
+int cErrors;
 
 typedef enum errorTypes ERROR;
 ERROR error = noerror;
 
-int found = 0;
 
-void PrintError(ERROR error, char *string)
-{
+//PrintError 함수: errorTypes 객체 내용을 바탕으로 ERROR 정보를 출력
+// error: illsp(허용되지 않는 문자 사용한 식별자 에러)
+//        illid(숫자와 함께 시작하는 식별자 에러)
+//        illlen(식별자길이idlen을 넘는 식별자 에러)
+//        overst(크기로 overflow 발생)
+
+
+void PrintError(enum errorTypes error, char* string) {
     int i;
     switch (error) {
-        case overst:
-            printf(" ***Error***      OVERFLOW ");
-            exit(0);
-            break;
-        case illsp:
-            printf(" ***Error***        %s            is illegal seperator \n", string);
-            break;
-        case illid:
-            printf(" ***Error***        ");
-            PrintID();
-            printf("        start with digit \n");
-            nextSTfree = nextid;
-            break;
-        case illlen:
-            printf(" ***Error***        ");
-            PrintID();
-            printf("        too long identifier \n");
-            break;
-        case wrongid:
-            printf(" ***Error***        ");
-            PrintID();
-            printf("       ");
-            for (i = nextid; i < nextSTfree - 1; i++)
+    case overst:
+        printf("***Error***      OVERFLOW");
+        ++cErrors;
+        exit(0);
+        break;
+    case illsp:
+        printf("***Error***\t\t\t%s illegal seperator", string);
+        ++cErrors;
+        break;
+    case illid:
+        printf("***Error***\t\t\t%s illegal IDENT", string);
+        ++cErrors;
+        break;
+    case illlen:
+        printf("***Error***\t\t\t%s over 10 words", string);
+        ++cErrors;
+        break;
+    case illchar:
+        printf("***Error***\t\t\t");
+        ++cErrors;
+        for (i = 0; i < strlen(string); i++) {
+            if (!isLetter(string[i]) && !isDigit(string[i]))
             {
-                if (!(isDigit(ST[i]) || isLetter(ST[i])))
-                    printf("%c ", ST[i]);
+                printf("%c", string[i]);
             }
-            printf("is not allowed\n");
-            nextSTfree = nextid;
-            break;
+        }
+        printf("illegal Character");
+        break;
     }
 }
