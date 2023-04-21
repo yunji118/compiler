1f377e99a42f4a57c73b77550506b29d40f537e0
Yunji Ha <yunji118@ewhain.net>
2023-04-21 20:12:25 +0900

    Update reporterror.c

--- reporterror.c
+++ reporterror.c
@@ -3,20 +3,24 @@
 #include "tn.h"
 #include "glob.h"
 
+
+extern void PrintError(enum errorTypes error, char* string);
+
+
+typedef enum errorTypes ERRORtypes;
+ERRORtypes error;
+
 extern void ReadID(char* string);
-extern void printerror(enum errorTypes error, char* string);
 
-typedef enum errorTypes ERROR;
-ERROR error;
-
-void reporterror(char* string) {
-    if(strlen(string)>10) {
+void ReportError(char* string) {
+    if (strlen(string) > 10) {
         error = illlen;
-        printerror(illlen, string);
-    } else {
+        PrintError(illlen, string);
+    }
+    else {
         //글자수 안 넘는 정상 길이 ident
-        printf("ident\t");
+        printf("ident\t\t");
         ReadID(string); //ST-index 출력
-        printf("\t%s”", string);    //읽은 토큰 출력
+        printf("\t\t%s", string);    //읽은 토큰 출력
     }
 }
