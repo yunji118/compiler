90ab92ed97e1f46fdf161fcb7feda70befffc900
Yunji Ha <yunji118@ewhain.net>
2023-04-21 20:11:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
+/*main.c - 전체 프로그램 실행
+* programmer - 김지윤, 김도연, 김원우, 하윤지
+* date - 2023/04/21
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "tn.h"
 #include "glob.h"
 
-int extern yylex();
-extern char* yytext;
-extern void printtoken(enum tnumber tn);
 
-int main()
+extern yylex();
+extern char* yytext;
+
+void extern printtoken(enum tokentypes tn);
+
+void main()
 {
-    enum tnumber tn;  // token number
+    nextid = 0;
+    nextSTfree = 0;
+    cErrors = 0;
+    line = 1;
+
+    enum tokentypes tn;  // token number
+    enum errorTypes error;
     line = 1;
 
     printf("Line number\tToken type\tST-index\tToken\n");
     while ((tn = yylex()) != TEOF) {
         printtoken(tn);
     }
-
     if (cErrors == 0) printf("No errors detected");
     else printf("%d errors detected\n", cErrors);
     printf("\n\n");
 
-    return 0;
 }
-
