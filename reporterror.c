e6861677209182f8bc0985e8b3df3da7ccc79ea9
Yunji Ha <yunji118@ewhain.net>
2023-05-10 00:27:17 +0900

    최종 reporterror.c

--- reporterror.c
+++ reporterror.c
@@ -1,17 +1,20 @@
+/* reporterror.c - identifier error 확인
+* programmer - 김지윤, 김도연, 김원우, 하윤지
+* date - 2023/04/26
+*/
 #include <stdio.h>
 #include <string.h>
 #include "tn.h"
 #include "glob.h"
 
-
 extern void PrintError(enum errorTypes error, char* string);
-
 
 typedef enum errorTypes ERRORtypes;
 ERRORtypes error;
 
 extern void ReadID(char* string);
 
+/*ReportError 함수 - 현재 들어온 string이 10글자 이내인지 확인 */
 void ReportError(char* string) {
     if (strlen(string) > 10) {
         error = illlen;
@@ -19,8 +22,8 @@ void ReportError(char* string) {
     }
     else {
         //글자수 안 넘는 정상 길이 ident
-        printf("ident\t\t");
         ReadID(string); //ST-index 출력
         printf("\t\t%s", string);    //읽은 토큰 출력
     }
 }
+
